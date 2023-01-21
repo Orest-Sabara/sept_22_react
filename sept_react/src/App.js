206ca87d1a416d88831aa79fbb02df712a5381f2
Orest-Sabara <2626orestsabara@gmail.com>
2023-01-21 19:50:12 +0200

    hw1_intro

---
+++ sept_react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {ImgBlock} from "./components/ImgBlock/ImgBlock";
+
+function App() {
+  return (
+    <div className="App">
+        <ImgBlock />
+    </div>
+  );
+}
+
+export default App;
