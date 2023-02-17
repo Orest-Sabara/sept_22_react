f09510a00f603b8f81166900a8e90a99d5bf006b
Orest-Sabara <2626orestsabara@gmail.com>
2023-02-17 19:21:29 +0200

    hw-5

--- sept_react/src/App.js
+++ sept_react/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import {ImgBlock} from "./components/ImgBlock/ImgBlock";
+import {Users, Posts} from "./components";
 
 function App() {
-    const url = 'https://static.onecms.io/wp-content/uploads/sites/6/2019/11/rick-and-morty-season-4-2000.jpg'
 
     return (
         <div className="App">
-            <ImgBlock url={url}/>
+            <Users />
+            <Posts />
         </div>
     );
 }
--- sept_react/src/components/ImgBlock/ImgBlock.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ImgBlock = ({url}) => {
-    return (
-        <div>
-            <img src={url} alt='rick happy'/>
-        </div>
-    )
-}
