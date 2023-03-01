742c680e9c12c0be6f281f7bbab11aee1f7e5e50
Orest-Sabara <2626orestsabara@gmail.com>
2023-03-01 23:45:12 +0200

    hw-5

--- sept_react/src/App.js
+++ sept_react/src/App.js
@@ -4,7 +4,7 @@ import {Users, Posts} from "./components";
 function App() {
 
     return (
-        <div className="App">
+        <div >
             <Users />
             <Posts />
         </div>
