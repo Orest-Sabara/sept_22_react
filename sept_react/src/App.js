b6e4cdec17ad7da349f271e78dfbf8c1aed06091
Orest-Sabara <2626orestsabara@gmail.com>
2023-03-01 23:56:52 +0200

    save

--- sept_react/src/App.js
+++ sept_react/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import {ImgBlock} from "./components/ImgBlock/ImgBlock";
 
 function App() {
-    const url = 'https://static.onecms.io/wp-content/uploads/sites/6/2019/11/rick-and-morty-season-4-2000.jpg'
-
     return (
         <div className="App">
-            <ImgBlock url={url}/>
+
         </div>
     );
 }
